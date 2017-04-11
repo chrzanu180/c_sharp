46427f8b2e6e4de58f52546c4636d01daba65738
chrzanu180 <chrzanu180@gmail.com>
2017-04-11 09:18:32 +0200

    Initial commit

---
+++ hello/hello/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace hello
+{
+   
+    public partial class Hello1 : Form
+    {
+        int progress = 0;
+        public Hello1()
+        {
+            InitializeComponent();
+        }
+
+        private void Hello1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (progress > 99)
+            {
+                progress = 100;
+            }
+            else
+            {
+                progress = progress + 10;
+                progressBar1.Value = progress;
+                textBox1.Text = progress.ToString();
+            }
+        }
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (progress < 1)
+            {
+                progress = 0;
+            }
+            else
+            {
+                progress = progress - 10;
+                progressBar1.Value = progress;
+                textBox1.Text = progress.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            int portsCnt = ports.Length;
+            //foreach (string port in ports)
+            //{
+                textBox1.Text = portsCnt.ToString();
+            //}
+
+
+
+        }
+    }
+}
---
+++ update/update/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace update
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PictureBox pb1 = new PictureBox();
+            pb1.ImageLocation = "../restart.png";
+            pb1.SizeMode = PictureBoxSizeMode.AutoSize;
+        }
+    }
+}
